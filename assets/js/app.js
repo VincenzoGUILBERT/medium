e9b2d2d474ff1c79d2688a6c7fbbf95471e26793
Vincent Guilbert <vincentguilbert13@protonmail.com>
2025-04-06 12:33:54 +0200

    Add navbar and sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,1 @@
 import '../styles/app.scss';
-
-const $ = require('jquery');
-require('bootstrap');
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
