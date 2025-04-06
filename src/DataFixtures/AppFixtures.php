d914f032f535082fb32d0db8f5d2991d4ae23826
Vincent Guilbert <vincentguilbert13@protonmail.com>
2025-04-06 12:58:10 +0200

    Install orm-fixtures and fakerphp/faker and add User dataset

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $hashedPassword = $this->hasher->hashPassword($user, 'password');
+            $user->setUsername($faker->userName())
+                ->setEmail($faker->email())
+                ->setPassword($hashedPassword)
+                ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
+            ;
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
