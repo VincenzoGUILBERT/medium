4400c2eb46d4f604efd28c35c479d2b050321cc8
Vincent Guilbert <vincentguilbert13@protonmail.com>
2025-04-06 11:46:21 +0200

    Switch from AssetMapper to Webpack Encore and install bootstrap and fontawesome

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import '../styles/app.scss';
+
+const $ = require('jquery');
+require('bootstrap');
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
